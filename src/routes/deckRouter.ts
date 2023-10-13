435d07676a837ffc122ee2ad18b6545116938e62
hannahellis4242 <hannahellis4242@gmail.com>
2023-10-13 23:30:07 +0100

    adding validation of user supplied parameters

--- src/routes/deckRouter.ts
+++ src/routes/deckRouter.ts
@@ -5,27 +5,29 @@ import { MersenneTwister19937, bool, shuffle } from "random-js";
 import { Hash } from "crypto";
 import stringToSeedArray from "../utils/stringToSeedArray";
 import Card from "../model/Card";
+import { DrawSchema } from "../model/Draw";
 
 const deckRouter = Router();
 deckRouter.get("/", (req, res) => {
+  const result = DrawSchema.safeParse(req.query);
+  if (!result.success) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(result.error.issues.map((issue) => `${issue}`).join("\n"));
+    return;
+  }
+  const { seed, num } = result.data;
   const time = Date.now();
-  const rawSeed = req.query.seed;
-  const seed = rawSeed ? rawSeed.toString() : time.toLocaleString();
-  const engine = MersenneTwister19937.seedWithArray(stringToSeedArray(seed));
+  const engine = MersenneTwister19937.seedWithArray(
+    stringToSeedArray(seed || time.toLocaleString())
+  );
   const reversal = bool();
   const deck = cards.map(
     ({ id, pip, suit }) => new Card(id, pip, suit, reversal(engine))
   );
   shuffle(engine, deck);
-  const rawNum = req.query.num;
-  if (!rawNum) {
+  if (!num) {
     res.json({ time, seed, deck });
-    return;
-  }
-  const numStr = rawNum.toString();
-  const num = parseInt(numStr);
-  if (isNaN(num)) {
-    res.sendStatus(StatusCodes.BAD_REQUEST);
     return;
   }
   const draw = deck.slice(0, num);
