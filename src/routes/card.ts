50a2b619760e204c847fd8d8996ab85c869f96b9
hannahellis4242 <hannahellis4242@gmail.com>
2023-10-14 14:46:35 +0100

    adding tests for the card api. Fixing some issues found and tidying up the schema to reflect these changes

--- src/routes/card.ts
+++ src/routes/card.ts
@@ -4,17 +4,22 @@ import { StatusCodes } from "http-status-codes";
 import { CardIdSchema } from "../model/CardId";
 
 const cardRouter = Router();
+cardRouter.get("/", (_, res) => {
+  res
+    .status(StatusCodes.BAD_REQUEST)
+    .type("text/plain")
+    .send("please provide a card index using url /card/{index}");
+});
 cardRouter.get("/:cardId", (req, res) => {
   const result = CardIdSchema.safeParse(req.params.cardId);
   if (!result.success) {
-    res
-      .status(StatusCodes.BAD_REQUEST)
-      .send(result.error.issues.map(({ message }) => message).join("\n"));
+    const error = result.error.issues.map(({ message }) => message).join("\n");
+    res.status(StatusCodes.BAD_REQUEST).type("text/plain").send(error);
     return;
   }
   const card = cards.at(result.data);
   if (!card) {
-    res.sendStatus(StatusCodes.BAD_REQUEST);
+    res.status(StatusCodes.BAD_REQUEST).send("out of range");
     return;
   }
   res.json(card);
--- test/api.test.ts
+++ test/api.test.ts
@@ -8,14 +8,37 @@ describe("api", () => {
       expect(response.statusCode).toBe(200);
       expect(response.body).toStrictEqual({ id: 0, pip: "0 The Fool" });
     });
-    test("must have a number", async () => {
-      const response = await request(app).get("/card");
-      expect(response.statusCode).toBe(404);
-    });
-    test("must be a number", async () => {
-      const response = await request(app).get("/card/hello");
-      expect(response.statusCode).toBe(400);
-      expect(response.body).toStrictEqual({}); //why???
+    describe("400 responses", () => {
+      [
+        [
+          "must have a number",
+          "/card",
+          "please provide a card index using url /card/{index}",
+        ],
+        ["must be a number", "/card/hello", "card id must be a number"],
+        [
+          "must be positive number",
+          "/card/-1",
+          "the card id must be positive or zero",
+        ],
+        [
+          "must be less than 78",
+          "/card/78",
+          "the card id must be less than 78",
+        ],
+        [
+          "must be a whole number",
+          "/card/51.5",
+          "the card id must be a whole number",
+        ],
+      ].forEach(([description, url, expected]) =>
+        test(description, async () => {
+          const response = await request(app).get(url);
+          expect(response.statusCode).toBe(400);
+          expect(response.type).toBe("text/plain");
+          expect(response.text).toBe(expected);
+        })
+      );
     });
   });
 });
